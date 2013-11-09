1df85b2c3569206726a6ff9213616cab64b70f01
px-amaac <amcintyre2@mail.csuchico.edu>
2013-11-09 14:50:24 -0800

    first commit

---
+++ p7/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+P7::Application.config.secret_key_base = '6d4be7316aa3e1dec03578056e5476ed748d969d15b42f7227f5cd672c99fd2d985d088a479206354501e4480a423f4ece35fa9e9806f7f9db6ccf788e4f0d7a'
---
+++ p7/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+P7::Application.config.session_store :cookie_store, key: '_p7_session'
