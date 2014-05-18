b74f8e17be9a4bfd2ab28de973548772cda4c0ec
px-amaac <amcintyre2@mail.csuchico.edu>
2014-05-18 17:46:39 -0400

    syncing code to transfer computers

--- p7/app/models/tag.rb
+++ p7/app/models/tag.rb
@@ -1,5 +1,6 @@
 class Tag < ActiveRecord::Base
   has_and_belongs_to_many :photos
+  default_scope -> { order('created_at DESC') }
   validates :value, length: { maximum: 20 }
-  validates :key, length: { maximum: 10 }
+  validates :key, length: { maximum: 20 }, uniqueness: true
 end
